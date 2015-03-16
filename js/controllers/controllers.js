d8a65d1652d7f9dd2a0faf2319e78eef6fe89f48
Zaiboot <zaiboot@gmail.com>
2015-03-16 14:42:51 -0600

    - Adding binding to ajax call.

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -44,32 +44,17 @@
 					dataBound : function() {
 						this.expandRow(this.tbody.find("tr.k-master-row").first());
 					},
-					dataSource: new kendo.data.DataSource( {
+					dataSource: {
 						transport: {
 							read: {
 								// the remote service url
 								url: "/data/demogrid.json",
-
-								// the request type
-								type: "get",
-
-								// the data type of the returned result
-								dataType: "json"
-
-								// additional custom parameters sent to the remote service
-								/*data: {
-									lat: 42.42,
-									lon: 23.20,
-									cnt: 10
-								}*/
+								
 							}
-						},
-						// describe the result format
-						schema: {
-							// the data, which the data source will be bound to is in the "list" field of the response
-							data: "list"
 						}
-					}),
+						// describe the result format
+
+					},
 				//Translate this.
 					columns : [{
                     field: "FirstName",
--- js/defaults.js
+++ js/defaults.js
@@ -2,17 +2,21 @@
 angular.module('defaults', []).value('gridDefaults' , {                
                 sortable: true,
                 pageable: true,
-				setDataSourceUrl: function(url) {
-					this.dataSource.transport.read = url;				
-				},
 				dataSource: {
-                    type: "odata",
-                    transport: {
-                        read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
-                    },
-                    pageSize: 5,
-                    serverPaging: true,
-                    serverSorting: true
-                }
-            }
+						transport: {
+							pageSize: 5,
+							serverPaging: true,
+							serverSorting: true,
+							read: {
+								url: "",
+								// the request type
+								type: "post",
+								// the data type of the returned result
+								dataType: "json"
+								
+							}
+						}
+
+					},
+				}
 			);
